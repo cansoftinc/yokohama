9beb8c5882ee7aed407d2a079c01c209fca11b93
duncan o.n <dunco20ny@gmail.com>
2014-08-06 12:40:09 -0700

    adding connection modified v1

--- connection.php
+++ connection.php
@@ -3,11 +3,23 @@
 //mysql_select_db('yokohamamotors',$connect);
 
 function getConnection() {
-	$dbhost = "127.8.253.130:3306";//"localhost:3306";
-	$dbuser = "adminAi8ZGBT";//"root";
-	$dbpass = "7XY9vyvz_-QA";//"123456";
-	$dbname = "yokohama";//yokohamamotors
-	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+	//$dbhost = "localhost:3306";
+	//$dbuser = "root";
+	//$dbpass = "123456";
+	//$dbname = "yokohamamotors";
+	//$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+	
+define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT')); 
+define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+define('DB_NAME',getenv('OPENSHIFT_GEAR_NAME'));
+
+$dsn = 'mysql:dbname='.DB_NAME.';host='.DB_HOST.';port='.DB_PORT;
+$dbh = new PDO($dsn, DB_USER, DB_PASS);
+	
+	
+	
 	$dbh -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	return $dbh;
 }
