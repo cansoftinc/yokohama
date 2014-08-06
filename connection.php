4f4914c46e5e3fbcb0bf2b514dccbe9132870cfe
duncan o.n <dunco20ny@gmail.com>
2014-08-06 12:32:02 -0700

    adding connection

--- connection.php
+++ connection.php
@@ -3,10 +3,10 @@
 //mysql_select_db('yokohamamotors',$connect);
 
 function getConnection() {
-	$dbhost = "localhost:3306";
-	$dbuser = "root";
-	$dbpass = "123456";
-	$dbname = "yokohamamotors";
+	$dbhost = "127.8.253.130:3306";//"localhost:3306";
+	$dbuser = "adminAi8ZGBT";//"root";
+	$dbpass = "7XY9vyvz_-QA";//"123456";
+	$dbname = "yokohama";//yokohamamotors
 	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
 	$dbh -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	return $dbh;
